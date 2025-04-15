87b5fd9f1d49793c2b83a066230b54ed17a7cbfd
kmndrr <kanderson@student.neumont.edu>
2025-04-14 22:42:28 -0600

    MadLibs
    final

---
+++ MadLibs/MadLibs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <random>
+using namespace std;
+
+#include <iostream>
+
+void initialize() {
+    string celebrity;
+    int number;
+    string disaster;
+
+    srand(static_cast<unsigned>(time(0)));
+    int randomNum = (rand() % 10);
+
+    cout << "Enter Celebrity: " << endl;
+    cin >> celebrity;
+    cout << endl;
+    cout << "Enter Number: ";
+    cin >> number;
+    cout << endl;
+    cout << "Enter a disaster: ";
+    cin >> disaster;
+    cout << endl;
+
+    cout << celebrity << " saved " << number << " refugees from a " << disaster << " " << randomNum << " times!!";
+
+}
+
+int main()
+{
+    initialize();
+}
+
